ce3efbf8a00624bce0e0d27cc8b2e76f3a006e5e
massa <zoumaniguimassa12@gmail.com>
2023-08-25 13:23:06 +0000

    Update app

--- app/controllers/greetings_controller.rb
+++ app/controllers/greetings_controller.rb
@@ -1,5 +1,5 @@
 class GreetingsController < ApplicationController
- def random
-   render json: { greeting: Greeting.order('RANDOM()').first.text }
- end
+  def random
+    render json: { greeting: Greeting.order('RANDOM()').first.text }
+  end
 end
